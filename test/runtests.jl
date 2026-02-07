54cfe63eb6132161e95c1f9c8fd28f335d005d57
Neven Sajko <4944410+nsajko@users.noreply.github.com>
2026-02-07 12:20:26 +0100

    test: add some (#3)

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,23 @@ using NonResizableVectors
 using Test
 
 @testset "NonResizableVectors.jl" begin
-    # Write your tests here.
+    @testset "subtyping" begin
+        for typ ∈ (MemoryVector, MemoryRefVectorImm, MemoryRefVectorMut)
+            @test typ <: AbstractVector
+            for elt ∈ (Float32, String)
+                @test typ{elt} <: AbstractVector{elt}
+            end
+        end
+    end
+    @testset "construction with `undef`" begin
+        for typ ∈ (MemoryVector, MemoryRefVectorImm, MemoryRefVectorMut)
+            for elt ∈ (Float32, String)
+                for n ∈ 0:4
+                    @test (@inferred typ{elt}(undef, n)) isa typ{elt}
+                end
+            end
+        end
+    end
 end
 
 using Aqua: Aqua
