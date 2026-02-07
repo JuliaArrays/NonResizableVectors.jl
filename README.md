f0d801be0ecdf14b38c081aea15af68f8a9ee0b9
Neven Sajko <s@purelymail.com>
2026-02-07 11:26:13 +0100

    init

--- README.md
+++ README.md
@@ -4,5 +4,21 @@
 [![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://JuliaArrays.github.io/NonResizableVectors.jl/dev/)
 [![Build Status](https://github.com/JuliaArrays/NonResizableVectors.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/JuliaArrays/NonResizableVectors.jl/actions/workflows/CI.yml?query=branch%3Amain)
 [![Coverage](https://codecov.io/gh/JuliaArrays/NonResizableVectors.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/JuliaArrays/NonResizableVectors.jl)
+[![Package version](https://juliahub.com/docs/General/NonResizableVectors/stable/version.svg)](https://juliahub.com/ui/Packages/General/NonResizableVectors)
+[![Package dependencies](https://juliahub.com/docs/General/NonResizableVectors/stable/deps.svg)](https://juliahub.com/ui/Packages/General/NonResizableVectors?t=2)
 [![PkgEval](https://JuliaCI.github.io/NanosoldierReports/pkgeval_badges/N/NonResizableVectors.svg)](https://JuliaCI.github.io/NanosoldierReports/pkgeval_badges/N/NonResizableVectors.html)
 [![Aqua](https://raw.githubusercontent.com/JuliaTesting/Aqua.jl/master/badge.svg)](https://github.com/JuliaTesting/Aqua.jl)
+
+Several simple subtypes of `AbstractVector` that just wrap `Memory` or `MemoryRef`.
+
+One of the main differences between these types and `Memory` is that the types defined in this package are supposed to never throw `BoundsError`. This is beneficial because throwing `BoundsError` escapes the `AbstractArray` value, making it impossible to eliminate the underlying allocation.
+
+Comparison with FixedSizeArrays.jl: the two packages are very similar, here are some differences:
+
+* This package only supports `AbstractVector`. This makes the types defined here cheaper, as there is no need to store the `AbstractArray` shape. It also simplifies the implementation.
+
+* This package supports some variations not supported by FixedSizeArrays.jl: a `MemoryRef` may be stored instead of directly storing a `Memory`. TODO: investigate whether this allows performance improvements.
+
+* The type parameters are executed differently, leading to a simpler implementation.
+
+In the future, it might make sense to extend FixedSizeArrays.jl so it would allow being based on the types defined here, instead of accepting only `Memory` as the parent type.
--- src/NonResizableVectors.jl
+++ src/NonResizableVectors.jl
@@ -1,5 +1,235 @@
+# TODO: test suite
+#
+# TODO: add methods for `Base.dataids`
+#
+# TODO: add methods for single-argument `Base.getindex`, if beneficial
+#
+# TODO: add methods for two-argument `Base.getindex` where the second argument is a `Colon`, if beneficial
+#
+# TODO: add methods for `Base.copy`, if beneficial
+#
+# TODO: add methods for destructuring interface (`Base.rest`), if beneficial
+#
+# TODO: add methods for destructuring interface (`Base.split_rest`), if beneficial
+#
+# TODO: add methods for destructuring interface (`Base.indexed_iterate`), if beneficial
+#
+# TODO: add methods for five-argument `Base.copyto!`, if beneficial
+#
+# TODO: add methods for two-argument `Base.copyto!`, if beneficial
+#
+# TODO: add methods for `Base.copy!`, if beneficial
+#
+# TODO: implement the Collects.jl interface: add methods for `function (::Collect)(::Type{V}, ::Any) where {V<:NonResizableVector}`
+#
+# TODO: implement the dense array interface/the strided array interface
 module NonResizableVectors
-
-# Write your package code here.
-
+    module VectorBoundsErrors
+        export VectorBoundsError, throw_vectorboundserror
+        mutable struct VectorBoundsError <: Exception
+            const type_of_vector::DataType
+            const first_index::Int
+            const last_index::Int
+            const index::Int
+            function VectorBoundsError(type_of_vector::DataType; first_index::Int, last_index::Int, index::Int)
+                @inline new(type_of_vector, first_index, last_index, index)
+            end
+        end
+        function Base.showerror(io::IO, ex::VectorBoundsError)
+            typ = ex.type_of_vector
+            fir = ex.first_index
+            las = ex.last_index
+            ind = ex.index
+            print(io, "VectorBoundsError: attempt to access a vector-like collection of type ")
+            show(io, typ)
+            print(io, ", with first index ")
+            show(io, fir)
+            print(io, ", with last index ")
+            show(io, las)
+            print(io, " at index [")
+            show(io, ind)
+            print(io, ']')
+            nothing
+        end
+        function throw_vectorboundserror(x, index::Int)
+            @inline let
+                typ = typeof(x)
+                first_index = firstindex(x)
+                last_index = lastindex(x)
+                ex = VectorBoundsError(typ; first_index, last_index, index)
+                throw(ex)
+            end
+        end
+    end
+    module CheckboundsOneBased
+        using ..VectorBoundsErrors
+        export checkbounds_one_based
+        function in_one_to(x::Int, m::Int)
+            @inline let
+                a = unsigned(x - one(x))
+                b = unsigned(m)
+                a < b
+            end
+        end
+        function checkbounds_one_based(::Type{Bool}, x::AbstractVector, index::Int)
+            @inline let
+                len = length(x)
+                in_one_to(index, len)
+            end
+        end
+        function checkbounds_one_based(x::AbstractVector, index::Int)
+            @inline let
+                is_inbounds = checkbounds_one_based(Bool, x, index)
+                if !is_inbounds
+                    throw_vectorboundserror(x, index)
+                end
+                nothing
+            end
+        end
+    end
+    module Miscellaneous
+        export vector_supertype, memory_type
+        const vector_supertype = AbstractVector  # TODO: consider switching to `DenseVector`
+        function memory_type(::Val{isatomic}, ::Type{T}, ::Val{addrspace}) where {isatomic, T, addrspace}
+            @inline GenericMemory{isatomic::Symbol, T, addrspace::Core.AddrSpace}
+        end
+    end
+    module GenericMemoryVectors
+        using ..Miscellaneous
+        export GenericMemoryVector, MemoryVector
+        struct GenericMemoryVector{isatomic, T, addrspace} <: vector_supertype{T}
+            memory::GenericMemory{isatomic, T, addrspace}
+            function GenericMemoryVector{isatomic, T, addrspace}(::UndefInitializer, n::Int) where {isatomic, T, addrspace}
+                @inline let
+                    mt = memory_type(Val(isatomic), T, Val(addrspace))
+                    memory = mt(undef, n)
+                    new(memory)
+                end
+            end
+        end
+        const MemoryVector = GenericMemoryVector{:not_atomic, T, Core.CPU} where {T}
+        function Base.size(x::MemoryVector)
+            @inline size(x.memory)
+        end
+        Base.@propagate_inbounds function Base.getindex(x::MemoryVector, index::Int)
+            @inline let
+                @boundscheck checkbounds(x, index)
+                @inbounds x.memory[index]
+            end
+        end
+        Base.@propagate_inbounds function Base.setindex!(x::MemoryVector, element, index::Int)
+            @inline let
+                @boundscheck checkbounds(x, index)
+                @inbounds x.memory[index] = element
+                x
+            end
+        end
+        Base.@propagate_inbounds function Base.isassigned(x::MemoryVector, index::Int)
+            @inline isassigned(x.memory, index)::Bool
+        end
+    end
+    module GenericMemoryRefVectors
+        using ..Miscellaneous
+        export
+            GenericMemoryRefVectorImm, GenericMemoryRefVectorMut, GenericMemoryRefVector,
+            MemoryRefVectorImm, MemoryRefVectorMut, MemoryRefVector
+        struct GenericMemoryRefVectorImm{isatomic, T, addrspace} <: vector_supertype{T}
+            memory_ref::GenericMemoryRef{isatomic, T, addrspace}
+            function GenericMemoryRefVectorImm{isatomic, T, addrspace}(::UndefInitializer, n::Int) where {isatomic, T, addrspace}
+                @inline let
+                    mt = memory_type(Val(isatomic), T, Val(addrspace))
+                    memory = mt(undef, n)
+                    memory_ref = memoryref(memory)
+                    new(memory_ref)
+                end
+            end
+        end
+        mutable struct GenericMemoryRefVectorMut{isatomic, T, addrspace} <: vector_supertype{T}
+            const memory_ref::GenericMemoryRef{isatomic, T, addrspace}
+            function GenericMemoryRefVectorMut{isatomic, T, addrspace}(::UndefInitializer, n::Int) where {isatomic, T, addrspace}
+                @inline let
+                    mt = memory_type(Val(isatomic), T, Val(addrspace))
+                    memory = mt(undef, n)
+                    memory_ref = memoryref(memory)
+                    new(memory_ref)
+                end
+            end
+        end
+        const GenericMemoryRefVector = Union{GenericMemoryRefVectorImm{isatomic, T, addrspace}, GenericMemoryRefVectorMut{isatomic, T, addrspace}} where {isatomic, T, addrspace}
+        const MemoryRefVector = GenericMemoryRefVector{:not_atomic, T, Core.CPU} where {T}
+        const MemoryRefVectorImm = GenericMemoryRefVectorImm{:not_atomic, T, Core.CPU} where {T}
+        const MemoryRefVectorMut = GenericMemoryRefVectorMut{:not_atomic, T, Core.CPU} where {T}
+        struct MemoryOffsetException <: Exception
+            offset::Int
+            function MemoryOffsetException(offset::Int)
+                @inline new(offset)
+            end
+        end
+        function validated_memory_ref(x::GenericMemoryRefVector)
+            @inline let
+                memory_ref = x.memory_ref
+                offset_into_memory = Base.memoryindex(memory_ref)
+                if offset_into_memory !== 1
+                    throw(MemoryOffsetException(offset_into_memory))
+                end
+                memory_ref
+            end
+        end
+        function Base.size(x::MemoryRefVector)
+            @inline let
+                memory_ref = validated_memory_ref(x)
+                size(memory_ref.mem)
+            end
+        end
+        Base.@propagate_inbounds function Base.getindex(x::MemoryRefVector, index::Int)
+            @inline let
+                @boundscheck checkbounds(x, index)
+                memory_ref = validated_memory_ref(x)
+                memory_ref_with_offset = @inbounds memoryref(memory_ref, index)
+                memory_ref_with_offset[]
+            end
+        end
+        Base.@propagate_inbounds function Base.setindex!(x::MemoryRefVector, element, index::Int)
+            @inline let
+                @boundscheck checkbounds(x, index)
+                memory_ref = validated_memory_ref(x)
+                memory_ref_with_offset = @inbounds memoryref(memory_ref, index)
+                memory_ref_with_offset[] = element
+                x
+            end
+        end
+        Base.@propagate_inbounds function Base.isassigned(x::MemoryRefVector, index::Int)
+            @inline let
+                if !checkbounds(Bool, x, index)
+                    return false
+                end
+                memory_ref = validated_memory_ref(x)
+                memory_ref_with_offset = @inbounds memoryref(memory_ref, index)
+                isassigned(memory_ref_with_offset)::Bool
+            end
+        end
+    end
+    using .CheckboundsOneBased, .GenericMemoryVectors, .GenericMemoryRefVectors
+    export
+        MemoryVector, MemoryRefVectorImm, MemoryRefVectorMut
+    const NonResizableVector = Union{MemoryVector{T}, MemoryRefVector{T}} where {T}
+    function Base.IndexStyle(::Type{<:NonResizableVector})
+        @inline IndexLinear()
+    end
+    function Base.checkbounds(::Type{Bool}, x::NonResizableVector, index::Int)
+        @inline checkbounds_one_based(Bool, x, index)
+    end
+    function Base.checkbounds(x::NonResizableVector, index::Int)
+        @inline checkbounds_one_based(x, index)
+    end
+    function Base.iterate(x::NonResizableVector, index)
+        @inline let
+            index = index::Int
+            if checkbounds(Bool, x, index)
+                ((@inbounds x[index]), index + 1)
+            else
+                nothing
+            end
+        end
+    end
 end
