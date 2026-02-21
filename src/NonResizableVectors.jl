a991bd21b97dc0a5cda1a06cb7f9cdf5026028bc
Neven Sajko <4944410+nsajko@users.noreply.github.com>
2026-02-21 19:36:31 +0100

    lose the `inline` annotations (#27)
    A real-world example (no-heap-allocation merge sort for sorting
    `NTuple` values) show that the `inline` annotations are not necessary
    in any of these combinations:
    
    * `MemoryVector` or `MemoryRefVectorImm` or `MemoryRefVectorMut`
    
    * Julia v1.12.5 or v1.13.0-beta2 or v1.14.0-DEV.1761

--- src/NonResizableVectors.jl
+++ src/NonResizableVectors.jl
@@ -24,7 +24,7 @@ module NonResizableVectors
         export vector_supertype, memory_type
         const vector_supertype = AbstractVector  # TODO: consider switching to `DenseVector`
         function memory_type(::Val{isatomic}, ::Type{T}, ::Val{addrspace}) where {isatomic, T, addrspace}
-            @inline GenericMemory{isatomic::Symbol, T, addrspace::Core.AddrSpace}
+            GenericMemory{isatomic::Symbol, T, addrspace::Core.AddrSpace}
         end
     end
     module GenericMemoryVectors
@@ -33,32 +33,26 @@ module NonResizableVectors
         struct GenericMemoryVector{isatomic, T, addrspace} <: vector_supertype{T}
             memory::GenericMemory{isatomic, T, addrspace}
             function GenericMemoryVector{isatomic, T, addrspace}(::UndefInitializer, n::Int) where {isatomic, T, addrspace}
-                @inline let
-                    mt = memory_type(Val(isatomic), T, Val(addrspace))
-                    memory = mt(undef, n)
-                    new(memory)
-                end
+                mt = memory_type(Val(isatomic), T, Val(addrspace))
+                memory = mt(undef, n)
+                new(memory)
             end
         end
         const MemoryVector = GenericMemoryVector{:not_atomic, T, Core.CPU} where {T}
         function Base.size(x::MemoryVector)
-            @inline size(x.memory)
+            size(x.memory)
         end
         Base.@propagate_inbounds function Base.getindex(x::MemoryVector, index::Int)
-            @inline let
-                @boundscheck checkbounds(x, index)
-                @inbounds x.memory[index]
-            end
+            @boundscheck checkbounds(x, index)
+            @inbounds x.memory[index]
         end
         Base.@propagate_inbounds function Base.setindex!(x::MemoryVector, element, index::Int)
-            @inline let
-                @boundscheck checkbounds(x, index)
-                @inbounds x.memory[index] = element
-                x
-            end
+            @boundscheck checkbounds(x, index)
+            @inbounds x.memory[index] = element
+            x
         end
         Base.@propagate_inbounds function Base.isassigned(x::MemoryVector, index::Int)
-            @inline isassigned(x.memory, index)::Bool
+            isassigned(x.memory, index)::Bool
         end
         function Base.parent(x::MemoryVector)
             x.memory
@@ -72,23 +66,19 @@ module NonResizableVectors
         struct GenericMemoryRefVectorImm{isatomic, T, addrspace} <: vector_supertype{T}
             memory_ref::GenericMemoryRef{isatomic, T, addrspace}
             function GenericMemoryRefVectorImm{isatomic, T, addrspace}(::UndefInitializer, n::Int) where {isatomic, T, addrspace}
-                @inline let
-                    mt = memory_type(Val(isatomic), T, Val(addrspace))
-                    memory = mt(undef, n)
-                    memory_ref = memoryref(memory)
-                    new(memory_ref)
-                end
+                mt = memory_type(Val(isatomic), T, Val(addrspace))
+                memory = mt(undef, n)
+                memory_ref = memoryref(memory)
+                new(memory_ref)
             end
         end
         mutable struct GenericMemoryRefVectorMut{isatomic, T, addrspace} <: vector_supertype{T}
             const memory_ref::GenericMemoryRef{isatomic, T, addrspace}
             function GenericMemoryRefVectorMut{isatomic, T, addrspace}(::UndefInitializer, n::Int) where {isatomic, T, addrspace}
-                @inline let
-                    mt = memory_type(Val(isatomic), T, Val(addrspace))
-                    memory = mt(undef, n)
-                    memory_ref = memoryref(memory)
-                    new(memory_ref)
-                end
+                mt = memory_type(Val(isatomic), T, Val(addrspace))
+                memory = mt(undef, n)
+                memory_ref = memoryref(memory)
+                new(memory_ref)
             end
         end
         const GenericMemoryRefVector = Union{GenericMemoryRefVectorImm{isatomic, T, addrspace}, GenericMemoryRefVectorMut{isatomic, T, addrspace}} where {isatomic, T, addrspace}
@@ -98,60 +88,48 @@ module NonResizableVectors
         struct MemoryOffsetException <: Exception
             offset::Int
             function MemoryOffsetException(offset::Int)
-                @inline new(offset)
+                new(offset)
             end
         end
         function memory_index(r)  # compat wrapper
-            @inline let
-                @static if hasproperty(Base, :memoryindex)
-                    Base.memoryindex(r)
-                else
-                    Base.memoryrefoffset(r)  # for older versions of Julia, fall back to the non-public functionality
-                end
+            @static if hasproperty(Base, :memoryindex)
+                Base.memoryindex(r)
+            else
+                Base.memoryrefoffset(r)  # for older versions of Julia, fall back to the non-public functionality
             end
         end
         function validated_memory_ref(x::GenericMemoryRefVector)
-            @inline let
-                memory_ref = x.memory_ref
-                offset_into_memory = memory_index(memory_ref)
-                if offset_into_memory !== 1
-                    throw(MemoryOffsetException(offset_into_memory))
-                end
-                memory_ref
+            memory_ref = x.memory_ref
+            offset_into_memory = memory_index(memory_ref)
+            if offset_into_memory !== 1
+                throw(MemoryOffsetException(offset_into_memory))
             end
+            memory_ref
         end
         function Base.size(x::MemoryRefVector)
-            @inline let
-                memory_ref = validated_memory_ref(x)
-                size(memory_ref.mem)
-            end
+            memory_ref = validated_memory_ref(x)
+            size(memory_ref.mem)
         end
         Base.@propagate_inbounds function Base.getindex(x::MemoryRefVector, index::Int)
-            @inline let
-                @boundscheck checkbounds(x, index)
-                memory_ref = validated_memory_ref(x)
-                memory_ref_with_offset = @inbounds memoryref(memory_ref, index)
-                memory_ref_with_offset[]
-            end
+            @boundscheck checkbounds(x, index)
+            memory_ref = validated_memory_ref(x)
+            memory_ref_with_offset = @inbounds memoryref(memory_ref, index)
+            memory_ref_with_offset[]
         end
         Base.@propagate_inbounds function Base.setindex!(x::MemoryRefVector, element, index::Int)
-            @inline let
-                @boundscheck checkbounds(x, index)
-                memory_ref = validated_memory_ref(x)
-                memory_ref_with_offset = @inbounds memoryref(memory_ref, index)
-                memory_ref_with_offset[] = element
-                x
-            end
+            @boundscheck checkbounds(x, index)
+            memory_ref = validated_memory_ref(x)
+            memory_ref_with_offset = @inbounds memoryref(memory_ref, index)
+            memory_ref_with_offset[] = element
+            x
         end
         Base.@propagate_inbounds function Base.isassigned(x::MemoryRefVector, index::Int)
-            @inline let
-                if !checkbounds(Bool, x, index)
-                    return false
-                end
-                memory_ref = validated_memory_ref(x)
-                memory_ref_with_offset = @inbounds memoryref(memory_ref, index)
-                isassigned(memory_ref_with_offset)::Bool
+            if !checkbounds(Bool, x, index)
+                return false
             end
+            memory_ref = validated_memory_ref(x)
+            memory_ref_with_offset = @inbounds memoryref(memory_ref, index)
+            isassigned(memory_ref_with_offset)::Bool
         end
         function Base.parent(x::MemoryRefVector)
             parent(x.memory_ref)
@@ -163,19 +141,17 @@ module NonResizableVectors
         MemoryVector, MemoryRefVectorImm, MemoryRefVectorMut
     const NonResizableVector = Union{MemoryVector{T}, MemoryRefVector{T}} where {T}
     function Base.IndexStyle(::Type{<:NonResizableVector})
-        @inline IndexLinear()
+        IndexLinear()
     end
     function Base.checkbounds(x::NonResizableVector, indices...)
-        @inline checkbounds_lightboundserror(x, indices...)
+        checkbounds_lightboundserror(x, indices...)
     end
     function Base.iterate(x::NonResizableVector, index = 1)
-        @inline let
-            index = index::Int
-            if checkbounds(Bool, x, index)
-                ((@inbounds x[index]), index + 1)
-            else
-                nothing
-            end
+        index = index::Int
+        if checkbounds(Bool, x, index)
+            ((@inbounds x[index]), index + 1)
+        else
+            nothing
         end
     end
     if isdefined(Base, :dataids)  # not public: https://github.com/JuliaLang/julia/issues/51753
